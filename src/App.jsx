ff358411fb2313ab3a807731a9771ea376ea8d37
aung-myat-oo <aungmyatoo2025@gmail.com>
2023-12-28 18:07:20 +0630

    push

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import ToCurrency from "./components/ToCurrency";
 const App = () => {
   return (
     <ContextProvider>
-      <div className="py-16 px-10 min-h-screen w-full bg-[#171826]">
-        <div className="bg-[#1F2032] p-10 rounded-lg">
+      <div className="py-16 px-5 sm:px-10 min-h-screen w-full bg-[#171826]">
+        <div className="bg-[#1F2032] px-5 sm:px-10 py-10  rounded-lg">
           <h1 className="text-white text-[44px] font-roboto">Money Exchange</h1>
           <div className="pt-10 grid grid-cols-1 sm:grid-cols-3 gap-5">
             <div className="sm:block hidden">
